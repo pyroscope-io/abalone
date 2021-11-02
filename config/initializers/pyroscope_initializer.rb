cc3eb114dee659500cab65999e25393313782d0f
Ryan Perry <rperry2174@gmail.com>
2021-11-01 17:30:29 -0700

    Remove print statement

--- config/initializers/pyroscope_initializer.rb
+++ config/initializers/pyroscope_initializer.rb
@@ -1,7 +1,5 @@
 require "pyroscope"
 
-pp "Proscope initializer process.pid: #{Process.pid}"
-
 Pyroscope.configure do |config|
   config.app_name = "abalone.ruby.server" # replace this with some name for your application
   config.server_address = "http://localhost:4040" # replace this with the address of your pyroscope server
