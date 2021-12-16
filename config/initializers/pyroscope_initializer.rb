8492510e89b12284678dfb8f1010d5050ce9cd77
Ryan Perry <rperry2174@gmail.com>
2021-12-16 12:25:27 -0800

    Add docker changes

--- config/initializers/pyroscope_initializer.rb
+++ config/initializers/pyroscope_initializer.rb
@@ -2,5 +2,5 @@ require "pyroscope"
 
 Pyroscope.configure do |config|
   config.app_name = "abalone.ruby.server" # replace this with some name for your application
-  config.server_address = ENV["IS_DOCKER"] ? "http://pyroscope:4040" : "http://localhost:4040" 
+  config.server_address = "http://pyroscope-server:4040"
 end
